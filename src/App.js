d2db5b741929cbc2f1dfa7d6590fb7848a1322ba
Hiyoshi <nobuaki.hiyoshi@autodesk.com>
2023-05-09 18:26:25 +0800

    Weapon Cards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import "./App.css";
-import ranked_logo from "./images/masters_icon.webp";
-import damage_logo from "./images/damage_icon.png";
+
+// Image imports
+import alternator_smg from "./images/smg/alternator.webp";
+import car_smg from "./images/smg/car.webp";
+import prowler_smg from "./images/smg/prowler.webp";
+import r99_smg from "./images/smg/r99.webp";
+import volt_smg from "./images/smg/volt.webp";
+
 import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Card
-          img={ranked_logo}
-          title="Ranked Progression"
-          body="Calculate how much ladder points do you need to progress to the next tier rank."
-        ></Card>
-        <Card
-          img={damage_logo}
-          title="DPS Calculator"
-          body="Calculate the damage per second a weapon in game does against different legends."
-        ></Card>
+        <Card img={r99_smg}></Card>
+        <Card img={alternator_smg}></Card>
+        <Card img={volt_smg}></Card>
+        <Card img={car_smg}></Card>
+        <Card img={prowler_smg}></Card>
       </header>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const cardFocusAnimation = keyframes`
+0% { transform:scale(1); box-shadow: 0 20px 40px rgba(0, 0, 0, 0.4); }
+100% {transform:scale(1.1); box-shadow: 0px 20px 40px rgba(0, 0, 175, 0.4);}
+`;
 
 const CardStyle = styled.div`
   margin: 1rem;
@@ -10,13 +15,27 @@ const CardStyle = styled.div`
   border-radius: 1rem;
   background: #121418;
   box-shadow: 0 20px 40px rgba(0, 0, 0, 0.4);
+
+  // Expand card on hover
+  &:hover {
+    animation-name: ${cardFocusAnimation};
+    animation-duration: 1s;
+    animation-iteration-count: 1;
+    animation-fill-mode: forwards;
+  }
+`;
+
+const ImageStyle = styled.img`
+  width: 100%;
+  height: 10rem;
+  object-fit: contain;
 `;
 
 function Card(props) {
   const { title, body, img } = props;
   return (
     <CardStyle>
-      <img src={img} className="App-logo" alt="logo" />
+      <ImageStyle src={img} alt="logo" />
       <h3>{title}</h3>
       <p>{body}</p>
     </CardStyle>
